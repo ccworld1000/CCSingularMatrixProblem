d8f3b0b8bfb76202293dcae645518a6fcf6889ac
CC (deng you hua) <ccworld1000@gmail.com>
2020-06-04 21:32:33 +0800

    CCAutoTag : inf

--- CCSingularMatrixProblem/ViewController.m
+++ CCSingularMatrixProblem/ViewController.m
@@ -14,13 +14,35 @@
 
 @implementation ViewController
 
+- (void) test1 {
+    CGAffineTransform t = CGAffineTransformMake(0, 0, 0, 0, 0, 0);
+    CGAffineTransform it = CGAffineTransformInvert(t);
+    NSLog(@"it : %@", NSStringFromCGAffineTransform(it));
+}
+
+- (void) test2 {
+    CGAffineTransform t1 = CGAffineTransformMake(1, 0, 0, 1, 0, 0);
+    NSLog(@"t1 : %@", NSStringFromCGAffineTransform(t1));
+    
+    CGAffineTransform t2 = CGAffineTransformTranslate(t1, INFINITY, INFINITY);
+    NSLog(@"t2 : %@", NSStringFromCGAffineTransform(t2));
+    
+    CGAffineTransform t3 = CGAffineTransformTranslate(t1, -INFINITY, -INFINITY);
+    NSLog(@"t3 : %@", NSStringFromCGAffineTransform(t3));
+    
+    CGAffineTransform t4 = CGAffineTransformTranslate(t1, -INFINITY, +INFINITY);
+    NSLog(@"t4 : %@", NSStringFromCGAffineTransform(t4));
+    
+    CGAffineTransform t5 = CGAffineTransformTranslate(t1, +INFINITY, -INFINITY);
+    NSLog(@"t5 : %@", NSStringFromCGAffineTransform(t5));
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    CGAffineTransform t = CGAffineTransformMake(0, 0, 0, 0, 0, 0);
-    CGAffineTransform it = CGAffineTransformInvert(t);
-    NSLog(@"it : %@", NSStringFromCGAffineTransform(it));
+    [self test1];
 }
 
 
